35fe0dcc9a5839ddf02e5111e07954bd6f063d94
Shujero <shujero.service@gmail.com>
2024-11-22 16:29:08 +0300

    first commit

---
+++ config/env/production/server.js
@@ -0,0 +1,17 @@
+
+
+module.exports = ({ env }) => ({
+    proxy: true,
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    url: env('https://siel-strapi.onrender.com'),
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
+    },
+  })
+  
